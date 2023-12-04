df67f177f2ef028e626be64fbfaac5ae45818920
Nishu1201 <146128049+Nishu1201@users.noreply.github.com>
2023-12-04 09:54:33 +0530

    Add files via upload

---
+++ BookStore/BookStore/src/main/java/com/BookStore/BookApp.java
@@ -0,0 +1,96 @@
+package com.BookStore;
+import java.util.List;
+import java.util.Optional;
+
+import org.hibernate.HibernateException;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+
+public class BookApp {
+
+			public static void main(String []a) {
+			EntityManagerFactory factory = null;
+				
+		   try { 
+			//connecting to database using persistence unit
+					factory  = Persistence.createEntityManagerFactory("mn");
+					EntityManager em = factory.createEntityManager();
+					
+					
+		System.out.println("-----WELCOME TO BOOKSTORE DATABASES-----");
+		  
+		  
+		BookStore book1 = new BookStore(1, "Whereabouts" , "Jhumpa lahiri", 599.0 , 30);
+		BookStore book2 = new BookStore(2, "The Cristmas Pig" , "JK Rowling", 699.0 , 12);
+		BookStore book3 = new BookStore(3, "Anamica" , "Suryakanth" ,  499.0 , 25);
+		BookStore book4 = new BookStore(4, "Area Of Darkness" , "V.S.Naipal" , 899.0 , 10 );
+		
+						
+
+		  BookDAO bDAO = new BookDAO(em);
+		  
+		  bDAO.createBookStore(book1);
+		  bDAO.createBookStore(book2);
+		  bDAO.createBookStore(book3);
+		  bDAO.createBookStore(book4);
+					
+		    System.out.println("  Data added successfully  ");
+					
+	System.out.println("---------------------------------------------");
+
+					int  newid = 3;
+					String newbookname ="The Lean Startup" ;
+					String newauthorname = "Eric Ries" ;
+					double newprice = 999.0;
+					int newstock = 45;
+			
+					
+			bDAO.UpdateBookStore(newid, newbookname, newauthorname, newprice, newstock);
+					
+			
+			System.out.println(" Data updated successfully ");
+			
+	     System.out.println("-----------------------------------------------");	
+			
+			System.out.println("  BookStore details based on the id  :");
+							
+							Optional<BookStore> bk1 = bDAO.getById(1);
+							System.out.println(bk1);
+							 
+							bDAO.getById(1);
+							
+		       System.out.println("  Details of all the bookstore  ");	
+							
+							List<BookStore> allbk = bDAO.getAll();
+			                System.out.println(allbk);
+							
+							System.out.println("  Data removes based on id : ");
+							
+							bDAO.removeById(1);
+							
+							System.out.println(" 1st record is removed ");
+
+							
+			System.out.println(" Data removed successfully  ");
+			
+		 System.out.println("-----------------------------------------------");	
+
+			
+						}
+		   
+				catch (HibernateException e) {
+							 e.printStackTrace();
+							// System.out.println(" Hibernate exception ");
+						}
+				catch (Exception e) {
+						 e.printStackTrace();
+						// System.out.println(" Exception ");
+						}
+
+		        	}
+		
+	
+}
